2fa05eb0c0097cf56b8bf0cf5c07f921b0820914
Ulyana Gaydukova <gaydukova.ulyana@maul.ru>
2020-10-20 21:44:30 +0300

    task2

--- src/ru/vsu/num24/Main.java
+++ src/ru/vsu/num24/Main.java
@@ -8,26 +8,24 @@ public class Main {
     public static void main(String[] args) {
         double[] box1 = new double[3];
         double[] box2 = new double[3];
-        inputSizes(box1);
-        inputSizes(box2);
+        readSizes(box1);
+        readSizes(box2);
         Arrays.sort(box1);
         Arrays.sort(box2);
-        printBox(box1);
-        printBox(box2);
         compareBox(box1, box2);
     }
     public static void compareBox(double[] box1, double[] box2){
-        if(box1[0] > box2[0] && box1[1] > box2[1] && box1[2] > box2[2]) System.out.println("Box 2 fits in 1.");
-        if(box1[0] < box2[0] && box1[1] < box2[1] && box1[2] < box2[2]) System.out.println("Box 1 fits in 2.");
-        else System.out.println("Boxes cannot be placed inside one another.");
+        Answer currentAnswer;
+        if(box1[0] > box2[0] && box1[1] > box2[1] && box1[2] > box2[2]) currentAnswer = Answer.BOX_2_FITS_IN_1;
+        else if(box1[0] < box2[0] && box1[1] < box2[1] && box1[2] < box2[2]) currentAnswer = Answer.BOX_1_FITS_IN_2;
+        else currentAnswer = Answer.BOXES_CANNOT_BE_PLACED_INSIDE_ONE_ANOTHER;
+        outputAnswer(currentAnswer);
     }
-    public static void printBox(double[] box){
-        for(int i = 0; i < box.length; i++){
-            System.out.print(box[i] + "   ");
-        }
-        System.out.println();
+    public static void outputAnswer(Answer currentAnswer) {
+        System.out.println(currentAnswer);
     }
-    public static void inputSizes(double[] box) {
+
+    public static void readSizes(double[] box) {
         System.out.println("Enter the dimensions of the box:");
         Scanner scn = new Scanner(System.in);
         for(int i = 0; i < 3; i++){
@@ -35,4 +33,4 @@ public class Main {
         }
     }
     }
-}
+
